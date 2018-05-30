16c040d75bff298c694763b0b473a1a57b11bada
Jung Hyoung Ho <hhjung1202@naver.com>
2018-05-30 16:34:07 +0900

    EDIT REVISE_VERSION.PY

--- reivse_version.py
+++ reivse_version.py
@@ -1,10 +1,6 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-import json
-import requests
-import wget
-import os.path
 import csv
 
 tf.set_random_seed(777)  # reproducibility
@@ -13,7 +9,7 @@ class Learning:
 	def __init__(self):
 		# AccPrice AccVolume open low high close ma boll rsi
 		self.seq_length = 5
-		self.data_dim = 5
+		self.data_dim = 9
 		self.hidden_dim = 10
 		self.output_dim = 1
 		self.learning_rate = 0.001
@@ -23,6 +19,11 @@ class Learning:
 		self.trainY = []
 		self.textX = []
 		self.textY= []
+
+		self.DataLoading()
+
+		self.X = tf.placeholder(tf.float32, [None, self.seq_length, self.data_dim])
+		self.Y = tf.placeholder(tf.float32, [None, self.output_dim])
 
 	def DataLoading(self):
 		data = []
@@ -59,86 +60,53 @@ class Learning:
 		return numerator / (denominator + 1e-7)
 
 	def build_arch(self):
-		with tf.variable_scope('LSTM1_layer'):
-            # Conv1, [batch_size, 20, 20, 256]
-			cell = tf.contrib.rnn.BasicLSTMCell(
-				num_units=self.hidden_dim, state_is_tuple=True, activation=tf.tanh)
-			self.lstm_output, self.lstm_states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
-			
-            assert self.lstm_output.get_shape() == [cfg.batch_size, 20, 20, 256]
+		cell = tf.contrib.rnn.BasicLSTMCell(num_units=self.hidden_dim, state_is_tuple=True, activation=tf.tanh)
 
-        # Primary Capsules layer, return [batch_size, 1152, 8, 1]
-        with tf.variable_scope('Fully_conn_layer1'):
-            Y_pred = tf.contrib.layers.fully_connected(self.lstm_output[:, output_dim], output_dim, activation_fn=None)
-            assert Y_pred.get_shape() == [2]
+		self.lstm_output, _states = tf.nn.dynamic_rnn(cell, self.X, dtype=tf.float32)
 
-    def loss(self):
-    	
+		self.Y_pred = tf.contrib.layers.fully_connected( self.lstm_output[:, self.output_dim], self.output_dim, activation_fn=None)
+
+	def loss(self):
+		self.Euclidian_loss = tf.reduce_sum(tf.square(self.Y_pred - self.Y))  # sum of the squares
+		self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
+		self.train = self.optimizer.minimize(self.Euclidian_loss)
+
+	def check(self):
+		self.targets = tf.placeholder(tf.float32, [None, self.output_dim])
+		self.predictions = tf.placeholder(tf.float32, [None, self.output_dim])
+		self.rmse = tf.sqrt(tf.reduce_mean(tf.square(self.targets - self.predictions)))
 
 	def run(self):
-# build a dataset
+		self.DataLoading()
+		self.build_arch()
+		self.loss()
+		self.check()
 
-print(len(y), len(x))
-for i in range(0, len(y) - seq_length):
-	_x = x[i:i + seq_length]
-	_y = y[i + seq_length]  # Next close price
-	print(_x, "->", _y)
-	dataX.append(_x)
-	dataY.append(_y)
+		with tf.Session() as sess:
+			init = tf.global_variables_initializer()
+			sess.run(init)
 
-# train/test split
-train_size = int(len(dataY) * 0.7)
-test_size = len(dataY) - train_size
-trainX, testX = np.array(dataX[0:train_size]), np.array(
-	dataX[train_size:len(dataX)])
-trainY, testY = np.array(dataY[0:train_size]), np.array(
-	dataY[train_size:len(dataY)])
+			# Training step
+			for i in range(self.iterations):
+				_, step_loss = sess.run([self.train, self.Euclidian_loss], feed_dict={
+					self.X: self.trainX, self.Y: self.trainY})
+				print("[step: {}] loss: {}".format(i, step_loss))
 
-# input place holders
-X = tf.placeholder(tf.float32, [None, seq_length, data_dim])
-Y = tf.placeholder(tf.float32, [None, output_dim])
+			# Test step
+			test_predict = sess.run(self.Y_pred, feed_dict={self.X: self.testX})
+			rmse_val = sess.run(self.rmse, feed_dict={
+				self.targets: self.testY, self.predictions: test_predict})
+			print("RMSE: {}".format(rmse_val))
 
-# build a LSTM network
-cell = tf.contrib.rnn.BasicLSTMCell(
-	num_units=hidden_dim, state_is_tuple=True, activation=tf.tanh)
-outputs, _states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
-#Y_pred = tf.contrib.layers.fully_connected(
-#   outputs[:, output_dim], output_dim, activation_fn=None)  # We use the last cell's output
-Y_pred = tf.contrib.layers.fully_connected(
-	outputs[:, output_dim], output_dim, activation_fn=None)  # We use the last cell's output
-# cost/loss
-loss = tf.reduce_sum(tf.square(Y_pred - Y))  # sum of the squares
-# optimizer
-optimizer = tf.train.AdamOptimizer(learning_rate)
-train = optimizer.minimize(loss)
+			# Plot predictions
+			print(self.testY)
+			print(test_predict)
+			plt.plot(self.testY, label='actual')
+			plt.plot(test_predict, label='predict')
+			plt.xlabel("Time Period")
+			plt.ylabel("Stock Price")
+			plt.show()
 
-# RMSE
-targets = tf.placeholder(tf.float32, [None, output_dim])
-predictions = tf.placeholder(tf.float32, [None, output_dim])
-rmse = tf.sqrt(tf.reduce_mean(tf.square(targets - predictions)))
-
-with tf.Session() as sess:
-	init = tf.global_variables_initializer()
-	sess.run(init)
-
-	# Training step
-	for i in range(iterations):
-		_, step_loss = sess.run([train, loss], feed_dict={
-								X: trainX, Y: trainY})
-		print("[step: {}] loss: {}". format(i, step_loss))
-
-	# Test step
-	test_predict = sess.run(Y_pred, feed_dict={X: testX})
-	rmse_val = sess.run(rmse, feed_dict={
-					targets: testY, predictions: test_predict})
-	print("RMSE: {}".format(rmse_val))
-
-	# Plot predictions
-	print(testY)
-	print(test_predict)
-	plt.plot(testY , label= 'actual' )
-	plt.plot(test_predict, label = 'predict')
-	plt.xlabel("Time Period")
-	plt.ylabel("Stock Price")
-	plt.show()
-
+if __name__ == "__main__":
+	base_Learning = Learning()
+	base_Learning.run()
